d9dc734368bbd5ad1a4ee7482997827096272d81
ronimour <ronimour@gmail.com>
2013-10-04 15:28:17 -0300

    Criado os modelos

---
+++ src/br/ufrn/ru_ufrn/model/Alimento.java
@@ -0,0 +1,45 @@
+package br.ufrn.ru_ufrn.model;
+
+public class Alimento {
+	private String nome;
+	private String descricao;
+	private String imagem;
+	
+	public Alimento() {
+		
+	}
+
+	public Alimento(String nome, String descricao, String imagem) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.imagem = imagem;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	
+	
+	
+}
---
+++ src/br/ufrn/ru_ufrn/model/Cardapio.java
@@ -0,0 +1,70 @@
+package br.ufrn.ru_ufrn.model;
+
+import java.util.List;
+
+public class Cardapio {
+	private List<Alimento> cafeDaManha;
+	private List<Alimento> almocoVegetariano;
+	private List<Alimento> almocoCarnivoro;
+	private List<Alimento> jantaVegetariana;
+	private List<Alimento> jantaCarnivora;
+	
+	public Cardapio() {
+	
+	}
+
+	public Cardapio(List<Alimento> cafeDaManha,
+			List<Alimento> almocoVegetariano, List<Alimento> almocoCarnivoro,
+			List<Alimento> jantaVegetariana, List<Alimento> jantaCarnivora) {
+		super();
+		this.cafeDaManha = cafeDaManha;
+		this.almocoVegetariano = almocoVegetariano;
+		this.almocoCarnivoro = almocoCarnivoro;
+		this.jantaVegetariana = jantaVegetariana;
+		this.jantaCarnivora = jantaCarnivora;
+	}
+
+	public List<Alimento> getCafeDaManha() {
+		return cafeDaManha;
+	}
+
+	public void setCafeDaManha(List<Alimento> cafeDaManha) {
+		this.cafeDaManha = cafeDaManha;
+	}
+
+	public List<Alimento> getAlmocoVegetariano() {
+		return almocoVegetariano;
+	}
+
+	public void setAlmocoVegetariano(List<Alimento> almocoVegetariano) {
+		this.almocoVegetariano = almocoVegetariano;
+	}
+
+	public List<Alimento> getAlmocoCarnivoro() {
+		return almocoCarnivoro;
+	}
+
+	public void setAlmocoCarnivoro(List<Alimento> almocoCarnivoro) {
+		this.almocoCarnivoro = almocoCarnivoro;
+	}
+
+	public List<Alimento> getJantaVegetariana() {
+		return jantaVegetariana;
+	}
+
+	public void setJantaVegetariana(List<Alimento> jantaVegetariana) {
+		this.jantaVegetariana = jantaVegetariana;
+	}
+
+	public List<Alimento> getJantaCarnivora() {
+		return jantaCarnivora;
+	}
+
+	public void setJantaCarnivora(List<Alimento> jantaCarnivora) {
+		this.jantaCarnivora = jantaCarnivora;
+	}
+	
+	
+	
+	
+}

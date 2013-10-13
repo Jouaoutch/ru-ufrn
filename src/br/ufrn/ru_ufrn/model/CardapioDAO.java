a7a25b0996e830cdf80c5e3a199a4f31496a675f
ronimour <ronimour@gmail.com>
2013-10-13 03:14:54 -0300

    Deletando classes desnecessárias

--- src/br/ufrn/ru_ufrn/model/CardapioDAO.java
+++
@@ -1,7 +0,0 @@
-package br.ufrn.ru_ufrn.model;
-
-import br.ufrn.ru_ufrn.model.dao.GenericDAO;
-
-public interface CardapioDAO extends GenericDAO<Cardapio, java.util.Date> {
-		public Cardapio findByData(java.util.Date data);
-}
